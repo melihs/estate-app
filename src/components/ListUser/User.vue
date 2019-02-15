e018d45508499f124816561f7e6d3ee4b7310928
melih sahin <melihsahin24@gmail.com>
2019-02-15 14:44:17 +0300

    created components

---
+++ src/components/ListUser/User.vue
@@ -0,0 +1,10 @@
+<template>
+    <tr>
+        <th>1</th>
+        <td>test kullanııcı</td>
+        <td>test lokasyon</td>
+        <td>12/04/2019</td>
+        <td>2 saat</td>
+        <td class="p-1"><button class="btn btn-sm btn-danger">Sil</button> <button class="btn btn-sm btn-warning">Düzenle</button></td>
+    </tr>
+</template>
---
+++ src/components/NewUser.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row justify-content-sm-center">
+        <form class="col-md-4 form-group">
+            <fieldset>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Adınız">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Soyadınız">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="E-posta">
+                </div>
+                <div class="form-group">
+                    <input type="tel" class="form-control" placeholder="telefon">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Parola">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Parola tekrar">
+                </div>
+                <button type="submit" class="btn btn-success pull-right">Kayıt</button>
+            </fieldset>
+        </form>
+    </div>
+</template>
